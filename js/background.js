ebc5596db5147981641bf7ebf844b8a146f689a3
Chris <burnandbass@gmail.com>
2017-11-17 14:10:15 +0200

    Added option to download Instagram stories;

--- js/background.js
+++ js/background.js
@@ -3,20 +3,41 @@
 const documentUrlPatterns = ['https://instagram.com/*', 'https://www.instagram.com/*'];
 
 chrome.contextMenus.create({
-    'title': 'Open in new tab',
-    'documentUrlPatterns': documentUrlPatterns,
-    'contexts': ['page', 'link'],
-    'onclick': onClick
+	'title': 'Open in new tab',
+	'documentUrlPatterns': documentUrlPatterns,
+	'contexts': ['page', 'link'],
+	'onclick': onClick
 });
 
+//--------------------------------------------------------------
+//	When click on media in default feed
+//--------------------------------------------------------------
 function onClick(event) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs.length && tabs[0]) {
-            const currentTabId = tabs[0]['id'];
-            chrome.tabs.sendMessage(currentTabId, 'processMediaUnderMouse', (response) => {
-                const mediaUrl = String(response.mediaUrl);
-                mediaUrl.length && chrome.tabs.create({ 'url': mediaUrl });
-            });
-        }
-    });
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+		if (tabs.length && tabs[0]) {
+			const currentTabId = tabs[0]['id'];
+			chrome.tabs.sendMessage(currentTabId, 'processMediaUnderMouse', (response) => {
+				const mediaUrl = String(response.mediaUrl);
+				openMediaInNewTab(mediaUrl);
+			});
+		}
+	});
+}
+
+//--------------------------------------------------------------
+//	Open in new tab when user right-click on stories
+//--------------------------------------------------------------
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.type === 'openMediaInNewTab') {
+		openMediaInNewTab(request.mediaUrl);
+	}
+});
+
+//--------------------------------------------------------------
+//	Open media in new tab
+//--------------------------------------------------------------
+function openMediaInNewTab(mediaUrl) {
+	if (mediaUrl && mediaUrl.length) {
+		chrome.tabs.create({'url': mediaUrl});
+	}
 }
--- js/content.js
+++ js/content.js
@@ -1,20 +1,55 @@
 (function (window, document, undefined) {
 
-    const mediaRegex = /img|video/gi;
-    let lastKnownMediaSource = '';
+	const mediaRegex = /img|video/gi;
+	let lastKnownMediaSource = '';
 
-    document.addEventListener('mousedown', (event) => {
-        if (event.button == 2) {
-            const elements = document.elementsFromPoint(event.clientX, event.clientY);
-            const media = elements.find(element => mediaRegex.test(element.tagName));
-            lastKnownMediaSource = media.getAttribute('src');
-        }
-    });
-    
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        if (request == 'processMediaUnderMouse') {
-            sendResponse({ mediaUrl: lastKnownMediaSource });
-        }
-    });
-    
+
+	const getMediaSrcUnderPoint = (pointX, pointY) => {
+		const elements = document.elementsFromPoint(pointX, pointY);
+		const media = elements.find(element => mediaRegex.test(element.tagName));
+		const mediaSources = media.querySelectorAll('source');
+
+
+		if (mediaSources.length) {
+			const mediaSource = mediaSources[0];
+			const mediaSourceUrl = mediaSource.getAttribute('src');
+			if (mediaSourceUrl && mediaSourceUrl.length) {
+				return mediaSourceUrl;
+			}
+		}
+
+		return media ? media.getAttribute('src') : '';
+	};
+
+	window.addEventListener('contextmenu', (event) => {
+		//Instagram Stories
+		if (event.defaultPrevented) {
+			const mediaSource = getMediaSrcUnderPoint(event.clientX, event.clientY);
+
+			if (mediaSource && mediaSource.length) {
+
+				chrome.runtime.sendMessage({
+					type: 'openMediaInNewTab',
+					mediaUrl: mediaSource
+				}, (response) => {
+
+				});
+
+			}
+		}
+	});
+
+	document.addEventListener('mousedown', (event) => {
+		if (event.button == 2) {
+			lastKnownMediaSource = getMediaSrcUnderPoint(event.clientX, event.clientY);
+		}
+	});
+
+	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		if (request == 'processMediaUnderMouse') {
+			sendResponse({mediaUrl: lastKnownMediaSource});
+		}
+	});
+
+
 })(window, document);
